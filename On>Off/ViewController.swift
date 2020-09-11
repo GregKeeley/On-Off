cb2226f0b266f86e899843346c065c4ae1a713c6
Greg Keeley <GregoryKeeley@pursuit.org>
2020-09-11 08:55:32 -0400

    Initial Commit

---
+++ On>Off/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On>Off
+//
+//  Created by Gregory Keeley on 9/11/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
